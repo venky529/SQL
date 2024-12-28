40cff482902929952f970b5e702230cc7a5969f8
Venkatesh Madabathula <100125082+venky529@users.noreply.github.com>
2024-12-28 15:53:09 +0530

    CREATE

---
+++ 1.Create_Table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE employee (
+	employee_id INT,
+	name VARCHAR (50),
+	manager_id INT
+);
+
+INSERT INTO employee 
+VALUES
+	(1, 'Liam Smith', NULL),
+	(2, 'Oliver Brown', 1),
+	(3, 'Elijah Jones', 1),
+	(4, 'William Miller', 1),
+	(5, 'James Davis', 2),
+	(6, 'Olivia Hernandez', 2),
+	(7, 'Emma Lopez', 2),
+	(8, 'Sophia Andersen', 2),
+	(9, 'Mia Lee', 3),
+	(10, 'Ava Robinson', 3);
